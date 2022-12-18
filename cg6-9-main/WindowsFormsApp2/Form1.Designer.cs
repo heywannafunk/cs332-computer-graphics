03d1b6d69cd4a8287a51500a7f7f40278024903d
Artyom Golenishchev <48377936+heywannafunk@users.noreply.github.com>
2022-12-18 08:48:29 +0300

    Add files via upload

---
+++ cg6-9-main/WindowsFormsApp2/Form1.Designer.cs
@@ -0,0 +1,203 @@
+﻿namespace WindowsFormsApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea7 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend7 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series7 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea8 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend8 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series8 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea9 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend9 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series9 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBoxRed = new System.Windows.Forms.PictureBox();
+            this.pictureBoxGreen = new System.Windows.Forms.PictureBox();
+            this.pictureBoxBlue = new System.Windows.Forms.PictureBox();
+            this.chartR = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.chartG = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.chartB = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxRed)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxGreen)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxBlue)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chartR)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chartG)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chartB)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.pictureBox1.Location = new System.Drawing.Point(579, 114);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(603, 469);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.LoadCompleted += new System.ComponentModel.AsyncCompletedEventHandler(this.PictureBox1_LoadCompleted);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(836, 614);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(142, 47);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Upload";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // pictureBoxRed
+            // 
+            this.pictureBoxRed.Location = new System.Drawing.Point(35, 114);
+            this.pictureBoxRed.Name = "pictureBoxRed";
+            this.pictureBoxRed.Size = new System.Drawing.Size(226, 150);
+            this.pictureBoxRed.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBoxRed.TabIndex = 2;
+            this.pictureBoxRed.TabStop = false;
+            // 
+            // pictureBoxGreen
+            // 
+            this.pictureBoxGreen.Location = new System.Drawing.Point(311, 114);
+            this.pictureBoxGreen.Name = "pictureBoxGreen";
+            this.pictureBoxGreen.Size = new System.Drawing.Size(226, 150);
+            this.pictureBoxGreen.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBoxGreen.TabIndex = 3;
+            this.pictureBoxGreen.TabStop = false;
+            // 
+            // pictureBoxBlue
+            // 
+            this.pictureBoxBlue.Location = new System.Drawing.Point(35, 330);
+            this.pictureBoxBlue.Name = "pictureBoxBlue";
+            this.pictureBoxBlue.Size = new System.Drawing.Size(226, 150);
+            this.pictureBoxBlue.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBoxBlue.TabIndex = 4;
+            this.pictureBoxBlue.TabStop = false;
+            // 
+            // chartR
+            // 
+            chartArea7.Name = "ChartArea1";
+            this.chartR.ChartAreas.Add(chartArea7);
+            legend7.Name = "Legend1";
+            this.chartR.Legends.Add(legend7);
+            this.chartR.Location = new System.Drawing.Point(8, 498);
+            this.chartR.Name = "chartR";
+            this.chartR.Palette = System.Windows.Forms.DataVisualization.Charting.ChartColorPalette.None;
+            this.chartR.PaletteCustomColors = new System.Drawing.Color[] {
+        System.Drawing.Color.Red};
+            series7.ChartArea = "ChartArea1";
+            series7.Legend = "Legend1";
+            series7.Name = "Series1";
+            this.chartR.Series.Add(series7);
+            this.chartR.Size = new System.Drawing.Size(564, 256);
+            this.chartR.TabIndex = 5;
+            this.chartR.Text = "chart1";
+            // 
+            // chartG
+            // 
+            chartArea8.Name = "ChartArea1";
+            this.chartG.ChartAreas.Add(chartArea8);
+            legend8.Name = "Legend1";
+            this.chartG.Legends.Add(legend8);
+            this.chartG.Location = new System.Drawing.Point(12, 774);
+            this.chartG.Name = "chartG";
+            this.chartG.Palette = System.Windows.Forms.DataVisualization.Charting.ChartColorPalette.None;
+            this.chartG.PaletteCustomColors = new System.Drawing.Color[] {
+        System.Drawing.Color.Lime};
+            series8.ChartArea = "ChartArea1";
+            series8.Legend = "Legend1";
+            series8.Name = "Series1";
+            this.chartG.Series.Add(series8);
+            this.chartG.Size = new System.Drawing.Size(564, 256);
+            this.chartG.TabIndex = 6;
+            this.chartG.Text = "chart2";
+            // 
+            // chartB
+            // 
+            chartArea9.Name = "ChartArea1";
+            this.chartB.ChartAreas.Add(chartArea9);
+            legend9.Name = "Legend1";
+            this.chartB.Legends.Add(legend9);
+            this.chartB.Location = new System.Drawing.Point(595, 774);
+            this.chartB.Name = "chartB";
+            this.chartB.Palette = System.Windows.Forms.DataVisualization.Charting.ChartColorPalette.None;
+            this.chartB.PaletteCustomColors = new System.Drawing.Color[] {
+        System.Drawing.Color.Blue};
+            series9.ChartArea = "ChartArea1";
+            series9.Legend = "Legend1";
+            series9.Name = "Series1";
+            this.chartB.Series.Add(series9);
+            this.chartB.Size = new System.Drawing.Size(564, 256);
+            this.chartB.TabIndex = 7;
+            this.chartB.Text = "chart3";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1200, 1155);
+            this.Controls.Add(this.chartB);
+            this.Controls.Add(this.chartG);
+            this.Controls.Add(this.chartR);
+            this.Controls.Add(this.pictureBoxBlue);
+            this.Controls.Add(this.pictureBoxGreen);
+            this.Controls.Add(this.pictureBoxRed);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxRed)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxGreen)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxBlue)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chartR)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chartG)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chartB)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBoxRed;
+        private System.Windows.Forms.PictureBox pictureBoxGreen;
+        private System.Windows.Forms.PictureBox pictureBoxBlue;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chartR;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chartG;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chartB;
+    }
+}
+
---
+++ cg6-9-main/WindowsFormsApp2/Form1.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                var dialog = new OpenFileDialog();
+                dialog.Filter = "PNG files|*.png| jpg files(*.jpg)|*.jpg| All files(*.*)|*.*";
+                if (dialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+                {
+                    pictureBox1.LoadAsync(dialog.FileName);
+                    //pictureBox1.ImageLocation = dialog.FileName;
+                }
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Error");
+            }
+        }
+
+        enum ColorChannel
+        {
+            R,G,B,
+        }
+
+        private Bitmap highlightChannel(ColorChannel channel, Image image)
+        {
+            Bitmap bitmap_image = new Bitmap(image);
+            for (int i = 0; i < bitmap_image.Width; i++)
+            {
+                for (int j = 0; j < bitmap_image.Height; j++)
+                {
+                    //get the pixel from the scrBitmap image
+                    var actualColor = bitmap_image.GetPixel(i, j);
+                    var valueAlpha = actualColor.A;
+                    switch (channel)
+                    {
+                        case ColorChannel.R:
+                            {
+                                var value = actualColor.R;
+                                var newColor = Color.FromArgb(valueAlpha, value, 0, 0);
+                                bitmap_image.SetPixel(i, j, newColor);
+                                break;
+                            }
+                        case ColorChannel.G:
+                            {
+                                var value = actualColor.G;
+                                var newColor = Color.FromArgb(valueAlpha, 0, value, 0);
+                                bitmap_image.SetPixel(i, j, newColor);
+                                break;
+                            }
+                        case ColorChannel.B:
+                            {
+                                var value = actualColor.B;
+                                var newColor = Color.FromArgb(valueAlpha, 0, 0, value);
+                                bitmap_image.SetPixel(i, j, newColor);
+                                break;
+                            }
+                        default: break;
+                    }
+                }
+            }
+            return bitmap_image;
+
+        }
+
+        private int[] createDataForChart(ColorChannel channel, Image image)
+        {
+            int[] color = new int[256];
+            Bitmap bitmap_image = new Bitmap(image);
+            for (int i = 0; i < bitmap_image.Width; i++)
+            {
+                for (int j = 0; j < bitmap_image.Height; j++)
+                {
+                    //get the pixel from the scrBitmap image
+                    var actualColor = bitmap_image.GetPixel(i, j);
+                    switch (channel)
+                    {
+                        case ColorChannel.R:
+                            {
+                                var value = actualColor.R;
+                                color[value]++;
+                                break;
+                            }
+                        case ColorChannel.G:
+                            {
+                                var value = actualColor.G;
+                                color[value]++;
+                                break;
+                            }
+                        case ColorChannel.B:
+                            {
+                                var value = actualColor.B;
+                                color[value]++;
+                                break;
+                            }
+                        default: break;
+                    }
+                }
+            }
+            return color;
+        }
+        
+
+        private void PictureBox1_LoadCompleted(Object sender, AsyncCompletedEventArgs e)
+        {
+            var redImage = highlightChannel(ColorChannel.R, pictureBox1.Image);
+            var greenImage = highlightChannel(ColorChannel.G, pictureBox1.Image);
+            var blueImage = highlightChannel(ColorChannel.B, pictureBox1.Image);
+
+            pictureBoxRed.Image = redImage;
+            pictureBoxGreen.Image = greenImage;
+            pictureBoxBlue.Image = blueImage;
+
+            var valuesRed = createDataForChart(ColorChannel.R, pictureBox1.Image);
+            var valuesGreen = createDataForChart(ColorChannel.G, pictureBox1.Image);
+            var valuesBlue = createDataForChart(ColorChannel.B, pictureBox1.Image);
+
+            this.chartR.Series.Clear();
+            this.chartG.Series.Clear();
+            this.chartB.Series.Clear();
+
+            for (int i = 0; i < 256; i++)
+            {
+                Series series = this.chartR.Series.Add(i.ToString());
+                series.Points.Add(valuesRed[i]);
+                series = this.chartG.Series.Add(i.ToString());
+                series.Points.Add(valuesGreen[i]);
+                series = this.chartB.Series.Add(i.ToString());
+                series.Points.Add(valuesBlue[i]);
+            }
+            
+        }
+
+
+    }
+}
